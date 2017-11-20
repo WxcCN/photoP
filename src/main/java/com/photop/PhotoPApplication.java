3ed13e6b44f703c16b35ccf01ce8e7c481f746ae
nova <357991863@qq.com>
2017-11-20 21:37:40 +0800

    first commit

---
+++ src/main/java/com/photop/PhotoPApplication.java
@@ -0,0 +1,12 @@
+package com.photop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoPApplication.class, args);
+	}
+}

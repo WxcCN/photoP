f1b897593127d5062ad360e64eb7bf44736de9c6
bjwangxingchen <bjwangxingchen@corp.netease.com>
2017-11-21 23:14:25 +0800

    增加目录

--- src/main/java/com/photop/controller/front/MainpageController.java
+++ src/main/java/com/photop/controller/front/MainpageController.java
@@ -1,10 +1,17 @@
 package com.photop.controller.front;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by bjwangxingchen on 2017/11/21.
  */
 @RestController
 public class MainpageController {
 
+    @RequestMapping(value = "/test")
+    public String test(){
+        return "hehe";
+    }
 
 }

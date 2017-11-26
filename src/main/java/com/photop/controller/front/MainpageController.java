07f7f8f298e4519314b4f3b94594acbb8e203e44
nova <357991863@qq.com>
2017-11-27 02:21:54 +0800

    完成增删改查逻辑

--- src/main/java/com/photop/controller/front/MainpageController.java
+++ src/main/java/com/photop/controller/front/MainpageController.java
@@ -1,17 +1,89 @@
 package com.photop.controller.front;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.photop.dao.Photo;
+import com.photop.dao.repositoty.PhotoRepositorym;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * Created by bjwangxingchen on 2017/11/21.
  */
 @RestController
+//@RequestMapping(value = "/front")
 public class MainpageController {
 
+    @Autowired
+    private PhotoRepositorym photoRepositorym;
+
     @RequestMapping(value = "/test")
-    public String test(){
+    public String test() {
         return "hehe";
     }
+
+    //获取最热列表
+    @GetMapping(value = "/hotPhotos")
+    public List<Photo> photoList() {
+        List<Photo> photoList = photoRepositorym.findAll();
+        if (null != photoList)
+            return photoList;
+        else return null;
+    }
+    //获取最新列表
+    @GetMapping(value = "/newPhotos")
+    public List<Photo> newphotoList() {
+        List<Photo> photoList = photoRepositorym.findAll();
+        if (null != photoList)
+            return photoList;
+        else return null;
+    }
+
+    //查询一个照片集
+    @GetMapping(value = "/photo/{id}")
+    public Photo photoGet(@PathVariable("id") Integer id) {
+        return photoRepositorym.findOne(id);
+    }
+
+    //插入一个照片集
+    @PostMapping(value = "/manage/photo")
+    public String photoAdd(@RequestParam(value = "name", required = false) String name,
+                           @RequestParam("url") String url) {
+        Photo photo = new Photo();
+        photo.setUrls(url);
+        photo.setName(name);
+        photo = photoRepositorym.save(photo);
+        if (null != photo)
+            return "success: id=" + photo.getId();
+        else return "hehe";
+    }
+
+    //删除
+    @DeleteMapping(value = "/manage/photo/{id}")
+    public String photoDel(@PathVariable("id") Integer id) {
+        try {
+            photoRepositorym.delete(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "success";
+    }
+
+
+    //更新
+    @PutMapping(value = "/manage/photo/{id}")
+    public String photoUpdat(@PathVariable("id") Integer id,
+                             @RequestParam(value = "name", required = false) String name,
+                             @RequestParam("url") String url) {
+        Photo photo = new Photo();
+        photo.setId(id);
+        photo.setUrls(url);
+        photo.setName(name);
+        photo = photoRepositorym.save(photo);
+        if (null != photo)
+            return "success: id=" + photo.getId();
+        else return "hehe";
+    }
+
 
 }
--- src/main/java/com/photop/dao/repositoty/PhotoRepositorym.java
+++ src/main/java/com/photop/dao/repositoty/PhotoRepositorym.java
@@ -1,7 +1,10 @@
 package com.photop.dao.repositoty;
 
+import com.photop.dao.Photo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 /**
  * Created by Nova on 17/11/27.
  */
-public interface PhotoRepositorym {
+public interface PhotoRepositorym extends JpaRepository<Photo,Integer>{
 }

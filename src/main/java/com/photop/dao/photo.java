f1d7707237ac89a5131e4f6bb8f2513d59a5832e
nova <357991863@qq.com>
2017-11-27 02:17:39 +0800

    完成增删改查逻辑

---
+++ src/main/java/com/photop/dao/photo.java
@@ -0,0 +1,10 @@
+package com.photop.dao;
+
+/**
+ * 照片集类
+ *
+ * @auther Nova wangxingchen
+ * @create 17/11/27
+ */
+public class photo {
+}
---
+++ src/main/java/com/photop/dao/repositoty/PhotoRepositorym.java
@@ -0,0 +1,7 @@
+package com.photop.dao.repositoty;
+
+/**
+ * Created by Nova on 17/11/27.
+ */
+public interface PhotoRepositorym {
+}
